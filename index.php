99a73ffd986baf6609489855e3d7206661fb0716
Wara Massa <waramassa70@gmail.com>
2020-11-24 23:03:59 +0100

    php2 exo4

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$age = 18;
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP2 exo4</title>
+</head>
+<body>
+    <?php
+      if ($age >= 18) { ?>
+        <p> Tu es majeur...</P>
+    <?php }
+    else { ?>
+        <p>tu n\'es pas majeur!!!</p>
+    <?php } ?>
+    
+    
+</body>
+</html>
